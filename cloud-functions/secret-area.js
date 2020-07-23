7cfb51092c26f7bac595317c609ad4e48de8a43b
Olukayode <olukayodeshokunbi@gmail.com>
2020-07-23 08:57:34 -0400

    Minor update on typo error

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -11,7 +11,7 @@ exports.handler = function(event, context, callback) {
         body = {}
     }
 
-    if (body.pasword == "javascript") {
+    if (body.password == "javascript") {
         callback(null, {
             statusCode: 200,
             body: secretContent
