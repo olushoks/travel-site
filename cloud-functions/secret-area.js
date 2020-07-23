bc2428969a19ee224c006e07fc20bb3b143b25e7
Olukayode <olukayodeshokunbi@gmail.com>
2020-07-23 08:53:20 -0400

    Updated cloud function to check password

--- cloud-functions/secret-area.js
+++ cloud-functions/secret-area.js
@@ -1,6 +1,26 @@
 exports.handler = function(event, context, callback) {
-    callback(null, {
-        statusCode: 200,
-        body: 'Welcome to the super secret area'
-    })
+    const secretContent = `
+    <h3>Welcome to the secret area</h3>
+    <p>Here we can tell you that sky is not always <strong>blue</strong>, and the sky is not always the limit</p>
+    `
+    let body
+
+    if (event.body) {
+        body = JSON.parse(event.body)
+    } else {
+        body = {}
+    }
+
+    if (body.pasword == "javascript") {
+        callback(null, {
+            statusCode: 200,
+            body: secretContent
+        })
+    } else {
+        callback(null, {
+            statusCode: 401,
+        })
+    }
+
+    
 }
